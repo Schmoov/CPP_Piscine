e44013f50b42a7dc67c3f4101a3d343b679c3d33
Paul Arden <parden@paul-f5Ar3s3.clusters.42paris.fr>
2025-03-01 17:56:32 +0100

    00_00 DONE

--- Mod_00/ex00/megaphone.cpp
+++ Mod_00/ex00/megaphone.cpp
@@ -7,11 +7,11 @@ int	main(int argc, char *argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	} else {
 		for (int i = 1; i < argc; i++) {
-			std::string s = argv[i];
-			for (std::string::iterator it=s.begin(); it != s.end(); it++) {
+			const std::string& s = argv[i];
+			for (std::string::const_iterator it=s.begin(); it != s.end(); it++) {
 				std::cout << static_cast<char> (std::toupper(*it));
 			}
 		}
-		std::cout << std::endl;
+		std::cout << '\n';
 	}
 }
