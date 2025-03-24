d9a5ae65aa376269b73b7b1ceac816d71202f19e
Paul Arden <parden@paul-f5Ar6s1.clusters.42paris.fr>
2025-03-24 19:05:41 +0100

    Clap me pls

---
+++ Mod_03/ex00/ClapTrap.cpp
@@ -0,0 +1,75 @@
+#include "ClapTrap.hpp"
+#include <string>
+#include <iostream>
+
+ClapTrap::ClapTrap() : name("Undefined"), hp(10), mp(10), atk(0)
+{
+	std::cerr << "Built default ClapTrap\n";
+}
+
+ClapTrap::ClapTrap(const ClapTrap& c) : name(c.name), hp(c.hp), mp(c.mp), atk(c.atk)
+{
+	std::cerr << "Built ClapTrap \"" << name << "\"\n";
+}
+
+ClapTrap::ClapTrap(std::string& name) : name(name), hp(10), mp(10), atk(0)
+{
+	std::cerr << "Built a copy of ClapTrap \"" << name << "\"\n";
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cerr << "Destroyed ClapTrap \"" << name << "\"\n";
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& c)
+{
+	if (this != &c) {
+		name = c.name;
+		hp = c.hp;
+		mp = c.mp;
+		atk = c.atk;
+	}
+	return *this;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (hp < 1) {
+		std::cerr << "ClapTrap \"" << name
+			<< "\" is too DEAD to attack\n";
+	} else if (mp < 1) {
+		std::cerr << "ClapTrap \"" << name
+			<< "\" is too TIRED to attack\n";
+	} else {
+		mp--;
+		std::cerr << "ClapTrap \"" << name
+			<< "\" attacks \"" << target
+			<< "\", causing " << atk << " points of damage!\n";
+	}
+}
+
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	hp = amount > hp ? 0 : hp - amount;
+	std::cerr << "ClapTrap \"" << name
+		<< "\" took " << amount << " points of damage!\n";
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (hp < 1) {
+		std::cerr << "ClapTrap \"" << name
+			<< "\" is too DEAD to repair\n";
+	} else if (mp < 1) {
+		std::cerr << "ClapTrap \"" << name
+			<< "\" is too TIRED to repair\n";
+	} else {
+		mp--;
+		std::cerr << "ClapTrap \"" << name
+			<< "\" repairs itself from " << hp
+			<< " to " << hp+amount << " hit points !\n";
+		hp+=amount;
+	}
+}
---
+++ Mod_03/ex00/ClapTrap.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+class ClapTrap {
+private:
+	std::string name;
+	int hp;
+	int mp;
+	int atk;
+
+public:
+	ClapTrap();
+	ClapTrap(const ClapTrap& c);
+	ClapTrap(std::string& name);
+	~ClapTrap();
+	ClapTrap& operator=(const ClapTrap& c);
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
+
