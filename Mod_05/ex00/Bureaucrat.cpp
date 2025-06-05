94e0cf324ea2b449bb8910601d004fd2fa361813
Paul Arden <parden@f6r2s11.clusters.42paris.fr>
2025-06-05 16:12:13 +0200

    started

---
+++ Mod_05/ex00/Bureaucrat.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{std::cerr << "Skill Issue\n";}
+Bureaucrat::~Bureaucrat()
+{std::cerr << "Skill Issue\n";}
+Bureaucrat::Bureaucrat(const Bureaucrat& other): name(other.name), grade(other.grade) {}
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{(void) other; std::cerr << "Skill Issue\n"; return *this;}
+
+const std::string& Bureaucrat::getName() const {return name;}
+int Bureaucrat::getGrade() const {return grade;}
+Bureaucrat::Bureaucrat(std::string name, int grade): name(name), grade(grade) {
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+void Bureaucrat::incr() {
+	grade--;
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+void Bureaucrat::decr() {
+	grade++;
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+std::ostream& operator<<(std::ostream& os, Bureaucrat& b) {
+	os << b.getName() << ", bureacrat grade " << b.getGrade() << '\n';
+	return os;
+}
