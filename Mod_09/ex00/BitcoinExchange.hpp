5229953ac95ab508c5109d2dff77e542e9922080
Paul Arden <parden@f1r5s17.clusters.42paris.fr>
2025-12-08 16:34:30 +0100

    working on 9

--- Mod_09/ex00/BitcoinExchange.hpp
+++ Mod_09/ex00/BitcoinExchange.hpp
@@ -1,9 +1,16 @@
 #pragma once
+#include <map>
+#include <string>
 
 class BitcoinExchange {
-public:
+private:
+	std::map<std::string, double> db;
 	BitcoinExchange();
 	BitcoinExchange(const BitcoinExchange& o);
 	BitcoinExchange& operator=(const BitcoinExchange& o);
+public:
+	BitcoinExchange(const std::string& path);
 	~BitcoinExchange();
 };
+
+std::map<std::string, double> initDb();
